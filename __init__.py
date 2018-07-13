b6de44c5f050c16658b558df363d3f6894091872
Franziska Horn <cod3licious@gmail.com>
2018-07-13 20:51:32 +0200

    add manifold learning experiments

--- __init__.py
+++ __init__.py
@@ -1,3 +1,3 @@
 from __future__ import unicode_literals, division, print_function, absolute_import
-from .simec import SimilarityEncoder, masked_mse, LastLayerReg
+from .simec import SimilarityEncoder, masked_mse, masked_binary_crossentropy, LastLayerReg
 from .utils import center_K
